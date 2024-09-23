5dcc803a3474e3c2deecc8e7cbcaffb3afea1231
Joshua Scott <joshua.ds.s019@gmail.com>
2024-09-23 12:11:11 -0300

    added and init styled About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <>My App</>;
+  return (
+    <>
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
+      <About />
+      {/* <Footer /> */}
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaGitSquare,
+  FaNpm,
+  FaReact,
+} from 'react-icons/fa';
+import { IoLogoJavascript } from 'react-icons/io5';
+import { RiTailwindCssFill } from 'react-icons/ri';
+
+const About = () => {
+  return (
+    <div className="flex flex-row justify-between m-auto w-3/4 text-xl">
+      <div className="p-10 mx-5 w-1/2">
+        <h1 className="text-4xl mb-10">About</h1>
+        <div className="flex flex-row">
+          <p className="w-1/2 mr-3">
+            Hello there,
+            <br /> I'm Joshua, a currently aspiring Frontend Developer and
+            eventually onto Fullstack. Despite the difficulty, I very much love
+            learning to code and program and make various creations from
+            interfaces, tools, and games.
+          </p>
+          <p className="w-1/2 ml-3">
+            This portfolio is where I'll be adding my available projects ranging
+            from humble beginnings to more robust and complex projects over
+            time, including my portfolio itself.
+          </p>
+        </div>
+      </div>
+
+      <div className="p-10 mx-5 w-1/2">
+        <h1 className="text-4xl mb-10">Technologies</h1>
+        <div className="flex flex-row text-6xl flex-wrap">
+          <FaHtml5 className="text-orange-600 m-6" />
+          <FaCss3Alt className="text-blue-900 m-6" />
+          <IoLogoJavascript className="text-yellow-400 m-6" />
+          <FaGitSquare className="text-orange-600 m-6" />
+          <FaNpm className="text-red-600 m-6" />
+          <FaReact className="text-cyan-400 m-6" />
+          <RiTailwindCssFill className="text-teal-600 m-6" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;

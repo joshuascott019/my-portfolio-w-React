7adc10977f525fcc949fa6770e4a7776573f909e
Joshua Scott <joshua.ds.s019@gmail.com>
2024-09-23 11:10:18 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Scans these files for Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

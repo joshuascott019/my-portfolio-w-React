d7117360d064856a39e1c94ecf7d465a765f6d7d
Joshua Scott <joshua.ds.s019@gmail.com>
2024-09-25 15:25:43 -0300

    rebuilt init navbar, no function yet

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <div className="bg-gray-950 text-white">
-      {/* <Navbar /> */}
+      <Navbar />
       <Hero />
       <About />
       <Footer />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between items-center w-3/4 m-auto p-6">
+      <h1 className="w-min text-blue-300 text-6xl font-bold tracking-tighter">
+        JOSHUA SCOTT
+      </h1>
+      <div>
+        <div>
+          <button className="m-8 text-3xl">About</button>
+          <button className="m-8 text-3xl">Portfolio</button>
+          <button className="m-8 text-3xl">Contact</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

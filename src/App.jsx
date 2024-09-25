e2cc4d92f47999761ec6a0fc91fa2eab9318349b
Joshua Scott <joshua.ds.s019@gmail.com>
2024-09-25 15:13:45 -0300

    rebuilt footer, no function yet to the links

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       {/* <Navbar /> */}
       <Hero />
       <About />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { FaLinkedin, FaGithub, FaFreeCodeCamp } from 'react-icons/fa';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-blue-600 flex flex-col p-8">
+      <div className="mx-auto my-5 text-white text-7xl flex space-x-20">
+        <FaLinkedin />
+        <FaGithub />
+        <FaFreeCodeCamp />
+      </div>
+      <p className="mx-auto my-3">Developed by: Joshua Scott</p>
+      <p className="mx-auto my-3">Designed by: Monica Marçal & Joshua Scott</p>
+    </div>
+  );
 };
 
 export default Footer;

c6a7c25b30b3f1431c1e74330844a2f7002fea75
Joshua Scott <joshua.ds.s019@gmail.com>
2024-09-28 11:05:13 -0300

    added links to project items and red highlight/shadow to items with no links

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,6 +8,7 @@ import {
 } from 'react-icons/fa';
 import { IoLogoJavascript } from 'react-icons/io5';
 import { RiTailwindCssFill } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const ProjectCard = ({ project }) => {
   const techs = project.techs;
@@ -16,9 +17,15 @@ const ProjectCard = ({ project }) => {
   const hasJavaScript = techs.includes('JavaScript');
   const hasReact = techs.includes('React');
   const hasTailwind = techs.includes('Tailwind');
+  const projectLink = project.projectLink;
+
+  const cardStyle =
+    projectLink === ''
+      ? 'flex flex-col justify-between text-center w-60 bg-red-300 rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-red-600'
+      : 'flex flex-col justify-between text-center w-60 bg-gray-100 rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-600';
 
   return (
-    <div className="flex flex-col justify-between text-center w-60 bg-gray-100 rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-600 hover:">
+    <Link to={projectLink} target="_blank" className={cardStyle}>
       <img
         className="object-cover h-3/4 border-b-4 border-blue-400"
         src={project.img}
@@ -38,7 +45,7 @@ const ProjectCard = ({ project }) => {
           <RiTailwindCssFill className="text-teal-600 shadow-xl" />
         ) : null}
       </div>
-    </div>
+    </Link>
   );
 };
 

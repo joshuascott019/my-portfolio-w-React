e22d6b7053313cf8ede59878d8dee0d080bc0a2d
Joshua Scott <joshua.ds.s019@gmail.com>
2024-10-01 10:18:22 -0300

    wip

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,15 +4,17 @@ import HeroImage from '../assets/images/HeroBanner.png';
 const Hero = () => {
   return (
     <div className="m-auto w-4/5 relative">
-      <div className="absolute top-1/2 right-[-100px] text-[3.8rem] tracking-[--10px] leading-[1.8em] -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-8xl mb-2.5 tracking-normal">Portfolio</h1>
-        <h1 className=" font-Montserrat w-min text-blue-300 text-9xl font-bold tracking-tighter">
+      <div className="absolute top-1/2 left-3/4 md:left-3/4 transform -translate-x-1/2 -translate-y-1/2">
+        <h1 className="text-4xl xl:text-8xl mb-2.5 tracking-normal">
+          Portfolio
+        </h1>
+        <h1 className=" font-Montserrat w-min text-blue-300 text-5xl xl:text-9xl font-bold tracking-tighter">
           JOSHUA SCOTT
         </h1>
       </div>
       <img
         src={HeroImage}
-        className="m-auto object-cover w-full h-[600px] border border-white border-l-0 border-r-0"
+        className="m-auto object-cover w-full h-[400px] xl:h-[600px] border border-white border-l-0 border-r-0"
       />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   const linkClass = ({ isActive }) =>
     isActive
-      ? 'font-Montserrat text-sm p-2 mx-2 md:text-2xl md:mx-4 md:p-3 xl:py-2 xl:px-4 xl:m-8 xl:text-3xl rounded-lg bg-blue-600'
-      : 'font-Montserrat text-sm p-2 mx-2 md:text-2xl md:mx-4 md:p-3 xl:py-2 xl:px-4 xl:m-8 xl:text-3xl hover:text-blue-600';
+      ? 'font-Montserrat text-sm p-2 mx-2 md:text-1xl md:mx-4 md:p-3 xl:py-2 xl:px-4 xl:m-8 xl:text-3xl rounded-lg bg-blue-600'
+      : 'font-Montserrat text-sm p-2 mx-2 md:text-1xl md:mx-4 md:p-3 xl:py-2 xl:px-4 xl:m-8 xl:text-3xl hover:text-blue-600';
 
   return (
-    <div className="flex justify-between items-center w-11/12 xl:w-4/5 m-auto p-2 xl:p-12">
-      <h1 className="font-Montserrat w-min text-blue-300 text-2xl md:text-4xl xl:text-6xl font-bold tracking-tighter">
+    <div className="flex justify-between items-center w-11/12 xl:w-4/5 m-auto p-6 xl:p-12">
+      <h1 className="font-Montserrat w-min text-blue-300 text-2xl md:text-4xl xl:text-6xl font-bold tracking-tighter leading-none">
         JOSHUA SCOTT
       </h1>
       <div>

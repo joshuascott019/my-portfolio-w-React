5cf586be0d5d82ae584f6a919f953e14dfaf39b9
Joshua Scott <joshua.ds.s019@gmail.com>
2024-10-07 10:24:44 -0300

    style: adjusted style to About component to display better on mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,18 +11,18 @@ import { RiTailwindCssFill } from 'react-icons/ri';
 
 const About = () => {
   return (
-    <div className="flex flex-col lg:flex-row justify-between mx-auto my-16 w-4/5 text-xl p-12">
-      <div className="p-10 mx-5 w-full lg:w-1/2">
+    <div className="flex flex-col gap-8 lg:flex-row justify-between mx-auto my-16 w-4/5 text-xl md:p-4">
+      <div className="md:p-4 mx-5 lg:w-1/2">
         <h1 className="font-Montserrat text-4xl mb-10">About</h1>
         <p className="w-full my-5 text-blue-300">Hello there,</p>
         <div className="flex flex-col lg:flex-row text-blue-300">
-          <p className="font-Montserrat w-full lg:w-1/2 my-3 lg:mr-3">
+          <p className="font-Montserrat lg:w-1/2 my-3 lg:mr-3">
             I'm Joshua, a currently aspiring Frontend Developer and eventually
             onto Fullstack. Despite the difficulty, I very much love learning to
             code and program and make various creations from interfaces, tools,
             and games.
           </p>
-          <p className="font-Montserrat w-full lg:w-1/2 my-3 lg:ml-3">
+          <p className="font-Montserrat lg:w-1/2 my-3 lg:ml-3">
             This portfolio is where I'll be adding my available projects ranging
             from humble beginnings to more robust and complex projects over
             time, including my portfolio itself.
@@ -30,9 +30,9 @@ const About = () => {
         </div>
       </div>
 
-      <div className="p-10 mx-5 w-full lg:w-1/2">
+      <div className="md:p-4 mx-5 lg:w-1/2">
         <h1 className="font-Montserrat text-4xl mb-10">Technologies</h1>
-        <div className="flex flex-row text-6xl flex-wrap">
+        <div className="flex flex-row justify-center lg:justify-normal text-6xl flex-wrap">
           <FaHtml5 className="text-orange-600 m-6" />
           <FaCss3Alt className="text-blue-900 m-6" />
           <IoLogoJavascript className="text-yellow-400 m-6" />

a06ed234f65774bf2a02bbcb3175cda336b838fe
Joshua Scott <joshua.ds.s019@gmail.com>
2024-10-19 09:59:22 -0300

    fix: redid intro text style: adjusted styles to About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,21 +11,21 @@ import { RiTailwindCssFill } from 'react-icons/ri';
 
 const About = () => {
   return (
-    <div className="flex flex-col gap-8 lg:flex-row justify-between mx-auto my-16 w-4/5 text-xl md:p-4">
+    <div className="flex flex-col gap-8 lg:flex-row justify-between mx-auto my-16 w-4/5 text-lg md:text-xl md:p-4">
       <div className="md:p-4 mx-5 lg:w-1/2">
         <h1 className="font-Montserrat text-4xl mb-10">About</h1>
         <p className="w-full my-5 text-blue-300">Hello there,</p>
         <div className="flex flex-col lg:flex-row text-blue-300">
           <p className="font-Montserrat lg:w-1/2 my-3 lg:mr-3">
-            I'm Joshua, a currently aspiring Frontend Developer and eventually
-            onto Fullstack. Despite the difficulty, I very much love learning to
-            code and program and make various creations from interfaces, tools,
-            and games.
+            I'm Joshua, a Frontend Developer with a passion for learning and
+            creating. I love transforming ideas into functional, interactive
+            interfaces. While I’m focusing on frontend technologies, I'm also
+            working towards becoming a Fullstack Developer.
           </p>
           <p className="font-Montserrat lg:w-1/2 my-3 lg:ml-3">
-            This portfolio is where I'll be adding my available projects ranging
-            from humble beginnings to more robust and complex projects over
-            time, including my portfolio itself.
+            This portfolio showcases my ongoing journey—from beginner projects
+            to more complex applications. Stay tuned for future updates as I
+            continue to grow and add new creations!
           </p>
         </div>
       </div>
@@ -33,13 +33,13 @@ const About = () => {
       <div className="md:p-4 mx-5 lg:w-1/2">
         <h1 className="font-Montserrat text-4xl mb-10">Technologies</h1>
         <div className="flex flex-row justify-center lg:justify-normal text-6xl flex-wrap">
-          <FaHtml5 className="text-orange-600 m-6" />
-          <FaCss3Alt className="text-blue-900 m-6" />
-          <IoLogoJavascript className="text-yellow-400 m-6" />
-          <FaGitSquare className="text-orange-600 m-6" />
-          <FaNpm className="text-red-600 m-6" />
-          <FaReact className="text-cyan-400 m-6" />
-          <RiTailwindCssFill className="text-teal-600 m-6" />
+          <FaHtml5 className="text-orange-600 m-6 hover:scale-110 transition-transform duration-300" />
+          <FaCss3Alt className="text-blue-900 m-6 hover:scale-110 transition-transform duration-300" />
+          <IoLogoJavascript className="text-yellow-400 m-6 hover:scale-110 transition-transform duration-300" />
+          <FaGitSquare className="text-orange-600 m-6 hover:scale-110 transition-transform duration-300" />
+          <FaNpm className="text-red-600 m-6 hover:scale-110 transition-transform duration-300" />
+          <FaReact className="text-cyan-400 m-6 hover:scale-110 transition-transform duration-300" />
+          <RiTailwindCssFill className="text-teal-600 m-6 hover:scale-110 transition-transform duration-300" />
         </div>
       </div>
     </div>

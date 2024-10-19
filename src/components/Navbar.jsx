bee52cd99a826ea67614a057bf94d9dbae38ee40
Joshua Scott <joshua.ds.s019@gmail.com>
2024-10-19 09:29:16 -0300

    style: adjusted style to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,21 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   const linkClass = ({ isActive }) =>
     isActive
-      ? 'font-Montserrat text-sm p-2 mx-2 md:text-1xl md:mx-4 md:p-3 xl:py-2 xl:px-4 xl:m-8 xl:text-3xl rounded-lg bg-blue-600'
-      : 'font-Montserrat text-sm p-2 mx-2 md:text-1xl md:mx-4 md:p-3 xl:py-2 xl:px-4 xl:m-8 xl:text-3xl hover:text-blue-600';
+      ? 'font-Montserrat text-sm p-2 mx-2 md:text-1xl md:mx-4 md:p-3 xl:py-2 xl:px-4 xl:m-8 xl:text-3xl rounded-lg bg-blue-600 text-white shadow-md'
+      : 'font-Montserrat text-sm p-2 mx-2 md:text-1xl md:mx-4 md:p-3 xl:py-2 xl:px-4 xl:m-8 xl:text-3xl hover:text-blue-600 transition-all duration-300';
 
   return (
-    <div className="flex justify-between items-center w-11/12 xl:w-4/5 m-auto p-6 xl:p-12">
+    <div className="flex justify-between items-center w-11/12 xl:w-4/5 m-auto p-6 xl:p-12 sticky top-0 bg-gray-950 shadow-lg z-10">
       <h1 className="font-Montserrat w-min text-blue-300 text-2xl md:text-4xl xl:text-6xl font-bold tracking-tighter leading-none">
         JOSHUA SCOTT
       </h1>
-      <div>
-        <div>
-          <NavLink to="/" className={linkClass}>
-            Home
-          </NavLink>
-          <NavLink to="/portfolio" className={linkClass}>
-            Portfolio
-          </NavLink>
-        </div>
+      <div className="flex space-x-2 md:space-x-4 xl:space-x-8">
+        <NavLink to="/" className={linkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/portfolio" className={linkClass}>
+          Portfolio
+        </NavLink>
       </div>
     </div>
   );

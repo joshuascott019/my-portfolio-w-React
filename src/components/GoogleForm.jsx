f1de8b89d5a478fa6321e921dd7405d67dba345c
Joshua Scott <joshua.ds.s019@gmail.com>
2024-11-27 09:00:49 -0300

    style: adjusted google form style

--- src/components/GoogleForm.jsx
+++ src/components/GoogleForm.jsx
@@ -1,9 +1,13 @@
 const GoogleForm = () => (
-  <div className="min-h-screen flex items-center justify-center">
+  <div className="min-h-screen flex items-center justify-center pt-24">
     <iframe
+      className="min-h-screen"
       src="https://docs.google.com/forms/d/e/1FAIpQLScrwHuYX3INl612y_5EOSPG3IcYu3Ji0I84jH9wlW6iuGC3rg/viewform?embedded=true"
       width="640"
-      height="719"
+      height="721"
+      frameBorder="0"
+      marginHeight="0"
+      marginWidth="0"
     >
       Loading…
     </iframe>
